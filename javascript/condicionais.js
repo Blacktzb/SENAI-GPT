1e01ea52e688943572217b8c30d8744aea960c4d
mikael <mikaelsantana249@gmail.com>
2025-08-25 21:48:47 -0300

    montei as pastas do javaScript e uns exercicios

---
+++ javascript/condicionais.js
@@ -0,0 +1,93 @@
+// Exercicio 1
+// let idade = prompt ("Qual o seu idade?");
+
+// if (idade >= 18) {
+//     // BLOCO VERDADEIRO
+//     alert ("Voce e maior de idade");
+
+// } else {
+//     // Bloco falso
+//     alert("Va pra super-nani!");
+
+// }
+
+// Exercicio 2
+
+// let numero = prompt ("Digite um numero");
+
+// if (numero >= 10) {
+
+//     alert ("o numero e positivo");
+
+// } else {
+
+//     alert ("o numero e negativo");
+// }
+
+// Exercicio 3
+
+// let numero = prompt ("Digite a nota do aluno");
+
+// if (numero >= 60) {
+
+//     alert ("Aprovado");
+
+// } else {
+    
+//     alert ("Reprovado");
+// }
+
+// Exercicio 4
+
+let numero = prompt ("Digite um numero");
+
+if (numero >= 0) {
+
+    alert ("o numero e positivo")
+
+ }  
+ 
+ if (numero <= 0) {
+
+    alert ("O numero e negatio")
+
+} else { 
+
+    alert ("zero")
+
+}
+
+
+
+
+
+
+
+// Exercicio 5
+
+// let idade = propt ("insira sua idade")
+
+// if  (idade > 0 && idade <= 12) {
+
+//     alert ("crianca.");
+
+// } else {
+
+//     if (idade > 12 && idade <= 17) {
+
+//         alert("adolecente.");
+//     }
+// }
+
+
+// Exercicio 6 
+
+// let num = prompt ("insira um numero:");
+
+// if (num % 2 == 0) {
+
+//     alert ("par")
+// } else { 
+
+//     alert ("impar")
+// }
