c9521496bdfb9987486c3817b4abf849fbfd6bed
mikael <mikaelsantana249@gmail.com>
2025-08-27 21:33:42 -0300

    atividade repeticoes

---
+++ javascript/repeticoes.js
@@ -0,0 +1,30 @@
+// for (let contadora = 1; contadora <= 5; contadora++) {
+
+//     console.log (contadora);
+// }
+ 
+// console.log ("While");
+// let contadoraWhile = 1;
+// while (contadoraWhile <= 5) { 
+
+//     console.log (contadoraWhile);
+
+//     contadoraWhile++;
+
+// } 
+ 
+// NIVEL BASICO
+
+// exercicio 1
+
+// for (let contadora = 1; contadora <= 10; contadora++) {
+
+//     console.log (contadora);
+// }
+
+// EXERCICIO 2
+
+let pergunta = prompt ("digita um numero")
+let 
+
+
