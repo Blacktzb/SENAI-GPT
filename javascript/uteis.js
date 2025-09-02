dfbf7bd09156bca2d340290c6c1f52395069d39b
mikael <mikaelsantana249@gmail.com>
2025-09-01 21:27:48 -0300

    finalizado as atividades

---
+++ javascript/uteis.js
@@ -0,0 +1,46 @@
+function demofilter() {
+
+    const numeros = [1, 2, 3, 4, 5, 6];
+
+    let numerosfiltrados = numeros.filter(numero => numero > 2);
+
+    console.log(numerosfiltrados)
+
+}
+
+function demofind() {
+
+    let produtos = [{
+        id: "1",
+        nome: "Teclado",
+        preco: 100
+    },
+    {
+        id: "2",
+        nome: "Mouse",
+        preco: 50
+
+    },
+    {
+        id: "3",
+        nome: "Monitor",
+        preco: 700
+
+    }
+];
+
+let produtoCaro = produtos.find(produto => produto.preco == 700);
+console.log(produtoCaro);
+
+}
+
+const demoMap = () => {
+
+const numeros = [1, 2, 3, 4, 5];
+
+
+const numerosDobrados = numeros.map(numero => numero * 2);
+console.log(numerosDobrados); 
+
+
+}
